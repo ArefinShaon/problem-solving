4d60aaa9381d697da59bcba83a318c4c0312f276
ArefinShaon <arefinshaon99@gmail.com>
2025-02-28 21:34:21 +0600

    arithmatic operation

---
+++ atithmatic.js
@@ -0,0 +1,14 @@
+let firstNum = 15;
+let secondNum = 10;
+
+let addition = firstNum + secondNum;
+let subtraction = firstNum - secondNum;
+let multiplication = firstNum * secondNum;
+let division = firstNum / secondNum;
+let modulas = firstNum % secondNum;
+
+console.log(addition);
+console.log(subtraction);
+console.log(multiplication);
+console.log(division);
+console.log(modulas);
