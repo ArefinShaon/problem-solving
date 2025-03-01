7c0c05f48f85bd449e7f3dd4492269658793b754
ArefinShaon <arefinshaon99@gmail.com>
2025-03-02 00:57:33 +0600

    if else done

---
+++ if.js
@@ -0,0 +1,15 @@
+let num = -6;
+if (num > 0) {
+    if (num == 2) {
+        console.log('Number is two');
+    }
+    else  {
+        console.log('Number is not two');
+    }
+}
+else if (num < 0) {
+    console.log('Number is negative');
+}
+else {
+    console.log('number is zero');
+}
