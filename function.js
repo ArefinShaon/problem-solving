47c934d15a67cb8fedd340f5b106e3689609f005
ArefinShaon <arefinshaon99@gmail.com>
2025-03-02 23:09:58 +0600

    function

---
+++ function.js
@@ -0,0 +1,21 @@
+function myName() {
+    console.log('My Name is Arefin');
+}
+myName();
+////////////////
+function primeNumber() {
+    return 7;
+    
+}
+
+let pNum = primeNumber();
+console.log(pNum);
+/////////////////////
+
+function sum(first, second) {
+    let add = first + second;
+    return add;
+}
+
+result=sum(7, 5);
+console.log(result);
