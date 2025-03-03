36c4ee070eaa2026a8dbf119643c7bcfc8931dd6
ArefinShaon <arefinshaon99@gmail.com>
2025-03-04 00:50:50 +0600

    loop done

---
+++ loop.js
@@ -0,0 +1,14 @@
+// While loop
+
+let num = 0;
+
+while (num < 10) {
+    num = num + 1;
+    console.log(`While Loop ${num}`);
+}
+
+// for loop
+
+for (let c = 0; c < 10; c++){
+    console.log(c);
+}
